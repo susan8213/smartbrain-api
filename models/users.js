e3b4088332ebb282064928d12eaeea69e6317a4c
Ying Hsiu Lai <susan8213@gmail.com>
2020-11-22 00:43:58 +0800

    Separate db client from models

--- models/users.js
+++ models/users.js
@@ -1,13 +1,4 @@
-const knex = require("knex");
-const client = knex({
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-  }
-});
+const client = require("./dbClient");
 
 const User = function(
   name,
